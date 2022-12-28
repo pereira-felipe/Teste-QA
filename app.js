95e25bda4d0a2f13eea3c1924095df506412fe8b
ana <ana@braip.comgit config --global user.email ana@braip.comgit config --global user.email ana@braip.com>
2022-12-28 13:57:35 -0300

    adicionando um console log

--- app.js
+++ app.js
@@ -2,3 +2,4 @@ console.log("Rodando o sistema de gerenciamento de séries")
 console.log("aplicação está se conectando ao banco de dados")
 // mais um console log de teste:
 console.log("A ana fez esse commit")
+console.log("Enviando para o GitHub")
