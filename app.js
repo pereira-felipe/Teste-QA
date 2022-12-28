a3890549eceabdee68bac079249b69534d835f58
ana <ana@braip.comgit config --global user.email ana@braip.comgit config --global user.email ana@braip.com>
2022-12-28 13:52:30 -0300

    console log indicando modificacoes da ana

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de séries")
 console.log("aplicação está se conectando ao banco de dados")
+// mais um console log de teste:
+console.log("A ana fez esse commit")
